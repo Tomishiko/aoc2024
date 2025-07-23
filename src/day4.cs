8bef66d6915fe8b1308424a212d4feba7697b882
Tomishiko <anton20435@outlook.com>
2025-07-23 17:28:02 +0300

    day4 full

--- src/day4.cs
+++ src/day4.cs
@@ -8,8 +8,40 @@ public static class day4
     public static void Run()
     {
 
-        string[] lines = File.ReadAllLines("./input/day4.test");
+        string[] lines = File.ReadAllLines("./input/day4");
         char[][] data = lines.Select(l => l.ToCharArray()).ToArray();
+        //Console.WriteLine(partOne(data));
+        Console.WriteLine(partTwo(data));
+    }
+
+    static int partTwo(char[][] data)
+    {
+        int count = 0;
+        int countM = 0;
+        (int, int)[] diags = { (-1, -1), (-1, 1), (1, -1), (1, 1) };
+
+        for (int i = 1; i < data.Length - 1; ++i)
+        {
+            for (int j = 1; j < data[i].Length - 1; ++j)
+            {
+                if (data[i][j] == 'A')
+                {
+                    foreach ((int di, int dj) in diags)
+                        if (data[i + di][j + dj] == 'M' && data[i-di][j-dj] == 'S')
+                            countM++;
+
+                    if (countM == 2 )
+                        count++;
+                    countM = 0;
+                }
+            }
+        }
+        return count;
+    }
+
+    static int partOne(char[][] data)
+    {
+
         int count = 0;
         for (int i = 0; i < data.Length; ++i)
         {
@@ -17,14 +49,13 @@ public static class day4
             {
                 if (data[i][j] == word[0])
                 {
-                    if (checkAdjacent(i, j, data))
-                        count++;
+                    checkAdjacent(i, j, data, ref count);
                 }
             }
         }
-        Console.WriteLine(count);
+        return count;
     }
-    static bool checkAdjacent(int i, int j, char[][] data)
+    static bool checkAdjacent(int i, int j, char[][] data, ref int count)
     {
 
         if (i >= data.Length)
@@ -38,10 +69,11 @@ public static class day4
             for (int dj = -1; dj <= 1; ++dj)
             {
                 if (!checkBoundaries(i, j, di, dj, data.Length, data[i].Length))
-                    return false;
+                    continue;
                 //Console.WriteLine($"i={i}, di={di}\nj={j}, dj={dj}\nli={data.Length}, lj = {data[i].Length}");
                 if (data[i + di][j + dj] == word[1])
-                    return tryWord(2, i + di, j + dj, di, dj, data);
+                    if (tryWord(2, i + di, j + dj, di, dj, data)) // if full word found
+                        count++;
             }
         }
 
